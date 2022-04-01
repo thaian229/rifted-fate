6592b9419cd2f17bbc2f952cc9ed82a53d90bc7c
Nguyễn Thái An <thaian229@gmail.com>
2022-04-01 09:58:50 +0700

    Refactoring + Adding new managers

---
+++ Assets/Scripts/Game/Managers/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.FPS.Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        // Singleton
+        public static GameManager instance;
+
+        // Use this for initialization
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else if (instance != null)
+            {
+                Destroy(this);
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SceneController.instance.SwitchScenes("MainMenu");
+        }
+    }
+}
+
---
+++ Assets/Scripts/Game/Managers/LevelManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unity.FPS.Game
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public Text scoreText;
+        public GameObject gameOverPanel;
+        public int score = 0;
+        public static LevelManager instance;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != null)
+            {
+                Destroy(this);
+            }
+
+            gameOverPanel.SetActive(false);
+            score = 0;
+            scoreText.text = "Score: " + score;
+        }
+
+        public void EarnScore(int amount)
+        {
+            score += amount;
+            scoreText.text = "Score: " + score;
+        }
+
+        public void GameOver()
+        {
+            Time.timeScale = 0;
+            gameOverPanel.SetActive(true);
+        }
+
+        public void GameOverContinue()
+        {
+            Time.timeScale = 1;
+            SceneController.instance.SwitchScenes("MainMenu");
+        }
+    }
+}
